e9eb59de09970681282ad335f0cebe3b9588d73d
Diego Souza <souza.diegoty@gmail.com>
2022-12-16 10:32:35 -0300

    Revert "Entendendo o uiatumatorviewer e selecionando o primeiro elemento"
    This reverts commit 0671f3ef5c7b48f4383fbd0072d996795a1377b5.

--- src/main/java/com/alura/appium/App.java
+++ src/main/java/com/alura/appium/App.java
@@ -11,6 +11,14 @@ import java.net.URL;
 
 public class App {
     public static void main(String[] args) throws MalformedURLException {
-        AppiumDriverConfig driver = new AppiumDriverConfig();
+        File apk = new File("src/main/resources/alura_esporte.apk");
+
+        DesiredCapabilities configuracoes = new DesiredCapabilities();
+        configuracoes.setCapability(MobileCapabilityType.APP, apk.getAbsolutePath());
+        configuracoes.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        configuracoes.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+
+        URL urlConexao = new URL("http://127.0.0.1:4723/wd/hub");
+        AppiumDriver driver = new AppiumDriver(urlConexao, configuracoes);
     }
 }
